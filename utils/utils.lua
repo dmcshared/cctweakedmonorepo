41f9cf0c14802932581d681270158e1cb78b4dc2
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2022-07-15 13:14:07 -0400

    add support for nested utils

--- utils/utils.lua
+++ utils/utils.lua
@@ -2,7 +2,21 @@ local out = {}
 
 function out.loadUtil(self, name)
     local util = require("disk/utils/" .. name)
-    self[name] = util
+
+    local result = {};
+    for match in (name .. "."):gmatch("(.-)[./]") do
+        table.insert(result, match);
+    end
+
+    for i = 1, (#result - 1) do
+        local key = result[i]
+        if not self[key] then
+            self[key] = {}
+        end
+        self = self[key]
+    end
+
+    self[result[#result]] = util
 end
 
 return out
